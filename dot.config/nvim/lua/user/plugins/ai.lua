8a57c413569759944ba4d2ed56bdfa9e3c6a1faf
Onur Ozuduru <onurozuduru@users.noreply.github.com>
2024-03-23 18:35:57 +0200

    Add settings for AI helper

---
+++ dot.config/nvim/lua/user/plugins/ai.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "Exafunction/codeium.vim",
+    event = "VeryLazy",
+
+    config = function()
+      vim.g.codeium_tab_fallback = ""
+      vim.g.codeium_no_map_tab = true
+      vim.g.codeium_disable_bindings = 1
+      vim.g.codeium_enabled = false
+
+      -- Disable codeium in buffer
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+        pattern = "*",
+        callback = function() vim.fn["codeium#command#Command"] "DisableBuffer" end,
+      })
+
+      -- Register keybindings to which-key for normal mode
+      require("which-key").register {
+        ["<leader>a"] = {
+          name = "󰟷 AI",
+          d = { "<cmd>Codeium Disable<cr>", "Disable Codeium" },
+          e = { "<cmd>Codeium Enable<cr>", "Enable Codeium" },
+          b = { "<cmd>Codeium DisableBuffer<cr>", "Buffer disable Codeium" },
+          B = { "<cmd>Codeium EnableBuffer<cr>", "Buffer enable Codeium" },
+          s = { "<cmd>lua print(vim.fn['codeium#GetStatusString']())<cr>", "Status" },
+        },
+      }
+
+      -- Highlight suggestions with different than comment
+      vim.cmd [[ hi CodeiumSuggestion guifg=#a9a999 gui=italic ]]
+    end,
+
+    -- Change keybindings for insert mode
+    keys = function()
+      return {
+        { "<M-Bslash>", mode = "i", false },
+        { "<M-]>",      mode = "i", false },
+        { "<M-[>",      mode = "i", false },
+        { "<C-]>",      mode = "i", false },
+        {
+          "<C-f>a",
+          function() return vim.fn["codeium#Accept"]() end,
+          mode = "i",
+          expr = true,
+          desc = "Codeium accept",
+        },
+        {
+          "<C-f>s",
+          function() return vim.fn["codeium#CycleCompletions"](1) end,
+          mode = "i",
+          expr = true,
+          desc = "Codeium next",
+        },
+        {
+          "<C-f>w",
+          function() return vim.fn["codeium#CycleCompletions"](-1) end,
+          mode = "i",
+          expr = true,
+          desc = "Codeium prev",
+        },
+        {
+          "<C-f>d",
+          function() return vim.fn["codeium#Clear"]() end,
+          mode = "i",
+          expr = true,
+          desc = "Codeium clear",
+        },
+      }
+    end,
+  },
+}
